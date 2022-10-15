42cdb4e40bf15d52a20c4c57fdff3bc13403e264
Kai Patel <00kaipatel@gmail.com>
2022-10-15 13:50:28 +0100

    Untested egl_hmap implementation

--- src/egl_hmap.c
+++ src/egl_hmap.c
@@ -1,11 +1,59 @@
 #include "egl_hmap.h"
+#include <stdint.h>
 #include <stdlib.h>
+
+#ifndef EAGLE_INTERNAL_HMAP_INITIAL_CAPACITY
+#define EAGLE_INTERNAL_HMAP_INITIAL_CAPACITY 509
+#endif
+
+/*
+ * Check whether a given integer is prime
+ * Time complexity O(sqrt(n))
+ * Avoids using early break
+ */
+
+static bool is_prime(size_t n) {
+  if (n <= 1)
+    return false;
+  if (n == 2)
+    return true;
+
+  for (size_t i = 1; i < n; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+
+    if (i * i >= n) {
+      break;
+    }
+  }
+
+  return true;
+}
+
+/*
+ * Compute the hash of a given pointer
+ * Uses Knuth's Mutiplicative Method
+ */
+
+static size_t hash(void *k, size_t capacity) {
+  uint64_t m = ((uint64_t)k * 2654435761) % ((uint64_t)1 << 32);
+  return m % capacity;
+}
 
 /*
  * Free all keys, values, and underlying data structures for a given egl_hmap
  */
 
-static void egl_hmap_free(struct egl_hmap *map) { return; }
+static void egl_hmap_free(struct egl_hmap *map) {
+  for (size_t i = 0; i < map->capacity; i++) {
+    free(map->array[i]->key);
+    free(map->array[i]->value);
+    free(map->array[i]);
+  }
+
+  free(map);
+}
 
 /*
  * Add a key and its value to a given egl_hmap
@@ -13,23 +61,74 @@ static void egl_hmap_free(struct egl_hmap *map) { return; }
  * Returns NULL if an error occurred
  */
 
-static void *egl_hmap_add(struct egl_hmap *map, void *key, void *value) {
-  return NULL;
+static void *egl_hmap_add(struct egl_hmap *map, void *key, void *value,
+                          int (*compare)(const void *, const void *)) {
+  size_t index = hash(key, map->capacity);
+
+  // Error if no space left
+  if (map->size == map->capacity) {
+    return NULL;
+  }
+
+  // Linear probing
+  while (map->array[index] != NULL) {
+    if (compare(map->array[index]->key, key) == 0) {
+      goto add;
+    }
+    index = (index + 1) % map->capacity;
+  }
+
+  map->array[index] = malloc(sizeof(struct egl_hmap_bucket));
+
+add:
+  map->array[index]->key = key;
+  map->array[index]->value = value;
+
+  map->size++;
+  return map->array[index]->key;
 }
 
 /*
  * Remove a key and its value from a given egl_hmap
+ * Removals are marked with a tombstone
+ * Tombstones are a NULL
  * Returns the removed key
  * Returns NULL if an error occurred
  */
 
-static void *egl_hmap_remove(struct egl_hmap *map, void *key) { return NULL; }
+static void *egl_hmap_remove(struct egl_hmap *map, void *key,
+                             int (*compare)(const void *, const void *)) {
+
+  size_t index = hash(key, map->capacity);
+
+  while (map->array[index] != NULL) {
+
+    if (compare(map->array[index]->key, key) == 0) {
+      void *ret = map->array[index]->key;
+      map->array[index]->key = NULL;
+      return ret;
+    }
+
+    index = (index + 1) % map->capacity;
+  }
+
+  return NULL;
+}
 
 /*
  * Test whether a given value exists in an egl_hmap
  */
 
-static bool egl_hmap_contains_value(struct egl_hmap *map, void *value) {
+static bool egl_hmap_contains_value(struct egl_hmap *map, void *value,
+                                    int (*compare)(const void *,
+                                                   const void *)) {
+  for (size_t i = 0; i < map->capacity; i++) {
+    if (map->array[i] != NULL) {
+      if (compare(map->array[i]->value, value) == 0) {
+        return true;
+      }
+    }
+  }
   return false;
 }
 
@@ -37,49 +136,79 @@ static bool egl_hmap_contains_value(struct egl_hmap *map, void *value) {
  * Test whether a given key exists in an egl_hmap
  */
 
-static bool egl_hmap_contains_key(struct egl_hmap *map, void *key) {
+static bool egl_hmap_contains_key(struct egl_hmap *map, void *key,
+                                  int (*compare)(const void *, const void *)) {
+  for (size_t i = 0; i < map->capacity; i++) {
+    if (map->array[i] != NULL) {
+      if (compare(map->array[i]->key, key) == 0) {
+        return true;
+      }
+    }
+  }
   return false;
 }
 
 /*
  * Get a value from an egl_hmap, given a key
+ * Returns NULL if the value is not found
+ * NB: Use the "contains_key" function to check whether the value is NULL, or
+ * whether no entry was found
  */
 
-static void *egl_hmap_get(struct egl_hmap *map, void *key) { return NULL; }
+static void *egl_hmap_get(struct egl_hmap *map, void *key,
+                          int (*compare)(const void *, const void *)) {
 
-/*
- * Get a pointer to the first element in an array of values of the egl_hmap
- * Returns NULL if an error occurred
- */
+  size_t index = hash(key, map->capacity);
 
-static void **egl_hmap_to_array(struct egl_hmap *map) { return NULL; }
+  while (map->array[index] != NULL) {
+    if (compare(map->array[index]->key, key) == 0) {
+      return map->array[index]->value;
+    }
+    index = (index + 1) % map->capacity;
+  }
+
+  return NULL;
+}
 
 /*
  * Initialise a newly created egl_hmap
+ * Set initial size to 0
+ * Allocate memory for "capacity" number of elements
  */
 
-static egl_hmap *egl_hmap_init(struct egl_hmap *map) {
+static egl_hmap *egl_hmap_init(struct egl_hmap *map, size_t capacity) {
   if (map == NULL)
     return NULL;
 
   map->size = 0;
+  map->array = calloc(capacity, sizeof(struct egl_hmap_bucket *));
+  map->capacity = capacity;
   map->add = &egl_hmap_add;
   map->remove = &egl_hmap_remove;
   map->contains_key = &egl_hmap_contains_key;
   map->contains_value = &egl_hmap_contains_value;
   map->get = &egl_hmap_get;
-  map->to_array = &egl_hmap_to_array;
-  map->free = egl_hmap_free;
+  map->free = &egl_hmap_free;
   return map;
 }
 
 /*
- * Creates a new, empty egl_hmap
- * Returns NULL if an error occurred
+ * Creates a new, empty egl_hmap of specified capacity
+ * If capacity is 0, use EAGLE_INTERNAL_HMAP_INITIAL_CAPACITY as default
+ * Panics if the specified capacity is not prime
+ * Returns NULL if an error occurred, or if the capacity is not prime
  */
 
-egl_hmap *egl_hmap_new(void) {
+egl_hmap *egl_hmap_new(size_t capacity) {
+  if (capacity == 0) {
+    capacity = EAGLE_INTERNAL_HMAP_INITIAL_CAPACITY;
+  }
+
+  if (!is_prime(capacity)) {
+    return NULL;
+  }
+
   egl_hmap *map = malloc(sizeof(egl_hmap));
-  map = egl_hmap_init(map);
+  map = egl_hmap_init(map, capacity);
   return map;
 }
--- src/egl_hmap.h
+++ src/egl_hmap.h
@@ -1,20 +1,35 @@
 #ifndef EGL_HMAP_H
 #define EGL_HMAP_H
 
-#include <stdbool.h>
 #include "egl_vector.h"
+#include <stdbool.h>
+
+/* Internal struct used to represent non-empty buckets
+ * Empty buckets are represented as NULL
+ */
+
+struct egl_hmap_bucket {
+  void *key;
+  void *value;
+};
 
 typedef struct egl_hmap {
   size_t size;
-  void *(*add)(struct egl_hmap *, void *, void *);
-  void *(*remove)(struct egl_hmap *, void *);
-  bool (*contains_key)(struct egl_hmap *, void *);
-  bool (*contains_value)(struct egl_hmap *, void *);
-  void *(*get)(struct egl_hmap *, void *);
-  void **(*to_array)(struct egl_hmap *);
+  size_t capacity;
+  void *(*add)(struct egl_hmap *, void *, void *,
+               int (*compare)(const void *, const void *));
+  void *(*remove)(struct egl_hmap *, void *,
+                  int (*compare)(const void *, const void *));
+  bool (*contains_key)(struct egl_hmap *, void *,
+                       int (*compare)(const void *, const void *));
+  bool (*contains_value)(struct egl_hmap *, void *,
+                         int (*compare)(const void *, const void *));
+  void *(*get)(struct egl_hmap *, void *,
+               int (*compare)(const void *, const void *));
+  struct egl_hmap_bucket **array;
   void (*free)(struct egl_hmap *);
 } egl_hmap;
 
-egl_hmap *egl_hmap_new(void);
+egl_hmap *egl_hmap_new(size_t);
 
 #endif
